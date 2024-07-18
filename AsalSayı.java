5f6a8396506b248c7e89234740acd9d980292aea
Olguhan Hünerli <olguhanhunerli@gmail.com>
2024-07-18 13:58:12 +0300

    Asal Sayi

--- AsalSayı.java
+++ AsalSayı.java
@@ -3,8 +3,8 @@ package Project;
 public class AsalSayı {
     public static void main(String[] args) {
         for (int i = 2 ; i<= 100 ; i++) {
-          if (i % 2 == 1) {
-              System.out.println(i);
+          if (i % 2 == 1 || i == 2) {
+              System.out.println( +i);
           }
         }
     }
